6f9f7864512240782919260e7ced1330a8f9c3f5
dongyunchao <dongyunchao369@163.com>
2020-02-06 00:24:29 +0800

    增加作用域

---
+++ DeepLearning_one_by_one/4-13-作用域与操作符的受限范围.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import tensorflow as tf
+tf.reset_default_graph()
+with tf.variable_scope("scope1") as sp:
+    var1 = tf.get_variable("v",[1])
+    
+print("sp:", sp.name)   #作用域名称
+print("var1:", var1.name)
+
+with tf.variable_scope("scope2"):
+    var2 = tf.get_variable("v", [1])
+    
+    with tf.variable_scope(sp) as sp1:
+        var3 = tf.get_variable("v3", [1])
+        
+        with tf.variable_scope(""):
+            var4 = tf.get_variable("v4", [1])
+        
+print("sp1:", sp1.name)
+print("var2:", var2.name)
+print("var3:", var3.name)
+print("var4:", var4.name)
+with tf.variable_scope("scope"):
+    with tf.name_scope("ba2"):
+        v = tf.get_variable("v", [1])   #v为一个变量
+        x = 1.0 + v                     #x为一个op
+        with tf.name_scope(""):
+            y = 1.0 + v
+print("v:", v.name)
+print("x.op:", x.op.name)
+print("y.op:",y.op.name)
